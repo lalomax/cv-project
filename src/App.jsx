84b0dc71ef64fccf1b00c93fbd884ccdef43e19b
Lalomax <lalomax365@gmail.com>
2024-01-05 15:29:40 -0400

    tik tok icon added

--- src/App.jsx
+++ src/App.jsx
@@ -49,14 +49,14 @@ function App() {
                 </div>
               </div>
               <div class="bg-white shadow rounded-lg mt-6 p-6">
-              <span class="text-gray-700 uppercase font-bold tracking-wider mb-2">
-                    Languages
-                  </span>
-                  <ul>
-                    {data.languages.map((language) => {
-                      return <li class="mb-2">{language}</li>;
-                    })}
-                  </ul>
+                <span class="text-gray-700 uppercase font-bold tracking-wider mb-2">
+                  Languages
+                </span>
+                <ul>
+                  {data.languages.map((language) => {
+                    return <li class="mb-2">{language}</li>;
+                  })}
+                </ul>
               </div>
             </div>
             {/* Second column */}
@@ -71,7 +71,7 @@ function App() {
                   <a
                     class="text-gray-700 hover:text-orange-600"
                     aria-label="Visit TrendyMinds LinkedIn"
-                    href=""
+                    href="https://www.linkedin.com/in/orlando-flores365/"
                     target="_blank"
                   >
                     <svg
@@ -85,7 +85,7 @@ function App() {
                       ></path>
                     </svg>
                   </a>
-                  <a
+                  {/* <a
                     class="text-gray-700 hover:text-orange-600"
                     aria-label="Visit TrendyMinds YouTube"
                     href=""
@@ -101,11 +101,11 @@ function App() {
                         d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
                       ></path>
                     </svg>
-                  </a>
+                  </a> */}
                   <a
                     class="text-gray-700 hover:text-orange-600"
                     aria-label="Visit TrendyMinds Facebook"
-                    href=""
+                    href="https://www.facebook.com/OrlandoFloresHuanca"
                     target="_blank"
                   >
                     <svg
@@ -122,7 +122,7 @@ function App() {
                   <a
                     class="text-gray-700 hover:text-orange-600"
                     aria-label="Visit TrendyMinds Instagram"
-                    href=""
+                    href="https://www.instagram.com/lalomax365/"
                     target="_blank"
                   >
                     <svg
@@ -139,7 +139,7 @@ function App() {
                   <a
                     class="text-gray-700 hover:text-orange-600"
                     aria-label="Visit TrendyMinds Twitter"
-                    href=""
+                    href="https://twitter.com/OrlandoFloresH"
                     target="_blank"
                   >
                     <svg
@@ -151,6 +151,24 @@ function App() {
                         fill="currentColor"
                         d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
                       ></path>
+                    </svg>
+                  </a>
+                  <a
+                    class="text-gray-700 hover:text-orange-600"
+                    aria-label="Visit TrendyMinds tiktok"
+                    href="https://www.tiktok.com/@lalomax365"
+                    target="_blank"
+                  >
+                    <svg
+                      
+                      
+                      class="h-6"
+                      
+                      viewBox="0 0 24 24"
+         
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path fill="currentColor" d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"></path>
                     </svg>
                   </a>
                 </div>

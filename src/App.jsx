984137ba600e08bd7ac0ac112e649f867f3ecac6
Lalomax <lalomax365@gmail.com>
2024-01-26 17:01:02 -0400

    Reverse de list order to appear meter first

--- src/App.jsx
+++ src/App.jsx
@@ -42,15 +42,22 @@ function App() {
                     Skills
                   </span>
                   <ul>
-                    {/* {data.skills.map((skill) => {
-                      return <li class="mb-2">{skill}</li>;
-                    })} */}
-                     {
-                    // console.log(Object.entries(data.languages))
-                    Object.entries(data.skills).map((skill) => {
-                      return <li class="mb-2">{skill[0]} <meter value={skill[1]} min="0" max="10" title={skill[1]}></meter></li>
-                    })
-                  }
+                    {
+                      // console.log(Object.entries(data.languages))
+                      Object.entries(data.skills).map((skill) => {
+                        return (
+                          <li class="mb-2">
+                            <meter
+                              value={skill[1]}
+                              min="0"
+                              max="10"
+                              title={skill[1]}
+                            ></meter>{" "}
+                            {skill[0]}
+                          </li>
+                        );
+                      })
+                    }
                   </ul>
                 </div>
               </div>
@@ -59,12 +66,20 @@ function App() {
                   Languages
                 </span>
                 <ul>
-                  {
-                    Object.entries(data.languages).map((language) => {
-                      // console.log(key,index)
-                      return <li class="mb-2">{language[0]} <meter value={language[1]} min="0" max="10" title={language[1]}></meter></li>
-                    })
-                  }
+                  {Object.entries(data.languages).map((language) => {
+                    // console.log(key,index)
+                    return (
+                      <li class="mb-2">
+                        <meter
+                          value={language[1]}
+                          min="0"
+                          max="10"
+                          title={language[1]}
+                        ></meter>{" "}
+                        {language[0]}
+                      </li>
+                    );
+                  })}
                 </ul>
               </div>
             </div>
@@ -169,15 +184,14 @@ function App() {
                     target="_blank"
                   >
                     <svg
-                      
-                      
                       class="h-6"
-                      
                       viewBox="0 0 24 24"
-         
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path fill="currentColor" d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"></path>
+                      <path
+                        fill="currentColor"
+                        d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"
+                      ></path>
                     </svg>
                   </a>
                 </div>
